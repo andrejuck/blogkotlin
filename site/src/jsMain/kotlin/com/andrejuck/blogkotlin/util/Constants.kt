441fd8e5584e69522ec8e0628bb13fddfdf5213a
André Juck <andrejuck@hotmail.com>
2024-05-03 18:26:13 -0300

    Creating Login Page UI

---
+++ site/src/jsMain/kotlin/com/andrejuck/blogkotlin/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.andrejuck.blogkotlin.util
+
+object Constants {
+    const val FONT_FAMILY = "Roboto"
+}
+
+object  Res {
+    object Image {
+        const val logo = "/favicon.ico"
+    }
+}
